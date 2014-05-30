90b8d5ba79ad596c792242fdbe0660a0a5c3a2f0
Carlos Matallín <matallo@vizzuality.com>
2014-05-30 18:53:46 +0200

    post to cartodb

---
+++ js/capture.js
@@ -0,0 +1,32 @@
+var Capture = {
+  initialize: function() {
+    this._initBindings();
+  },
+
+  _initBindings: function() {
+    var that = this;
+
+    $('.save').on('click', function(e) {
+      e.preventDefault();
+
+      that._onClick();
+    });
+  },
+
+  _onClick: function() {
+    var account = "trailnotes",
+        sql = "INSERT INTO untitled_table (name, description, image, the_geom) VALUES ('"+$('input').val()+"', '"+$('textarea').val()+"', '"+$('#picture').attr('src')+"', ST_SetSRID(ST_Point(39.474216404351694, -0.3733205795288086),4326))";
+
+    $.ajax({
+      url: "http://"+account+".cartodb.com/api/v2/sql?q="+sql+"&api_key=8c6d85e2e65aba7a23bf4bd8d7dfe029345e9db5",
+      type: "POST",
+      success: function(data) {
+        alert("HURACÁN!")
+      }
+    });
+  }
+}
+
+$(function() {
+  Capture.initialize();
+});
